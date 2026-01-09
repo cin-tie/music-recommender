4354cd8a9caf08a3ef4ef91fae4cd816b6045068
cin-tie <csizegod@gmail.com>
2026-01-10 01:23:27 +0300

    Revert "Filter writed, working?"
    This reverts commit 554add2bea272f83b84f52dbf511a9760611c6e4.

--- api-gateway/src/main/java/com/cintie/musicrecommender/apigateway/filters/JwtAuthenticationFilter.java
+++ api-gateway/src/main/java/com/cintie/musicrecommender/apigateway/filters/JwtAuthenticationFilter.java
@@ -1,8 +1,6 @@
 package com.cintie.musicrecommender.apigateway.filters;
 
-import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
@@ -17,29 +15,6 @@ public class JwtAuthenticationFilter implements WebFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        String path = exchange.getRequest().getURI().getPath();
-
-        if(path.startsWith("/auth/")){
-            return chain.filter(exchange);
-        }
-
-        String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
-        if(authHeader == null || !authHeader.startsWith("Bearer ")){
-            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-            return exchange.getResponse().setComplete();
-        }
-
-        String token = authHeader.substring(7);
-        try{
-            Jwts.parserBuilder()
-                    .setSigningKey(secret)
-                    .build()
-                    .parseClaimsJws(token);
-
-            return chain.filter(exchange);
-        } catch (Exception e){
-            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-            return exchange.getResponse().setComplete();
-        }
+        return null;
     }
 }
