063c77507bc2dd12a6114467b236a3d36ea60d7d
cin-tie <csizegod@gmail.com>
2026-01-10 00:24:58 +0300

    Empty auth filter

--- api-gateway/src/main/java/com/cintie/musicrecommender/apigateway/filters/JwtAuthenticationFilter.java
+++ api-gateway/src/main/java/com/cintie/musicrecommender/apigateway/filters/JwtAuthenticationFilter.java
@@ -1,4 +1,20 @@
 package com.cintie.musicrecommender.apigateway.filters;
 
-public class JwtAuthenticationFilter {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtAuthenticationFilter implements WebFilter {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        return null;
+    }
 }
