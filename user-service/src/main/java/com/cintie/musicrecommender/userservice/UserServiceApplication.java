cfa3de3da2bddb4a7ce863a12ec0cadb8e0c031f
cin-tie <csizegod@gmail.com>
2026-01-10 14:18:34 +0300

    Writed application.yml

--- user-service/src/main/java/com/cintie/musicrecommender/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/cintie/musicrecommender/userservice/UserServiceApplication.java
@@ -1,5 +1,11 @@
 package com.cintie.musicrecommender.userservice;
 
-@Spring
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class UserServiceApplication {
+    public void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 }
