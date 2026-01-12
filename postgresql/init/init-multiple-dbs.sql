f230e1070e513bc696be206d2fed6b8b83d85a5d
cin-tie <csizegod@gmail.com>
2026-01-12 17:38:23 +0300

    Changet postgres init

--- postgresql/init/init-multiple-dbs.sql
+++ postgresql/init/init-multiple-dbs.sql
@@ -6,3 +6,11 @@ CREATE USER user_user WITH ENCRYPTED PASSWORD 'user_pass';
 
 GRANT ALL PRIVILEGES ON DATABASE auth_db TO auth_user;
 GRANT ALL PRIVILEGES ON DATABASE user_db TO user_user;
+
+\c auth_db
+GRANT USAGE, CREATE ON SCHEMA public TO auth_user;
+ALTER SCHEMA public OWNER TO auth_user;
+
+\c user_db
+GRANT USAGE, CREATE ON SCHEMA public TO user_user;
+ALTER SCHEMA public OWNER TO user_user;
