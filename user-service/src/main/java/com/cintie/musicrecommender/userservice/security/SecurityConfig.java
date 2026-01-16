c07bd28c193d1e58f475631ca9ca1be5d5efd31e
cin-tie <csizegod@gmail.com>
2026-01-17 00:26:30 +0300

    No creation sessions

--- user-service/src/main/java/com/cintie/musicrecommender/userservice/security/SecurityConfig.java
+++ user-service/src/main/java/com/cintie/musicrecommender/userservice/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -25,7 +26,10 @@ public class SecurityConfig {
                 )
                 .addFilterBefore(
                     internalServiceJwtFilter, UsernamePasswordAuthenticationFilter.class
-                );
+                )
+                .sessionManagement(session -> session.
+                        sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         return httpSecurity.build();
     }
 }
