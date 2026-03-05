f2e354d835a00b497e22496e562e54c514e60f41
cin-tie <csizegod@gmail.com>
2026-03-05 14:06:20 +0300

    Writed cache service for RecommendationService

--- spotify-service/src/main/java/com/cintie/musicrecommender/spotifyservice/services/SpotifyRecommendationService.java
+++ spotify-service/src/main/java/com/cintie/musicrecommender/spotifyservice/services/SpotifyRecommendationService.java
@@ -1,5 +1,7 @@
 package com.cintie.musicrecommender.spotifyservice.services;
 
+import com.cintie.musicrecommender.spotifyservice.clients.AuthServiceClient;
+import com.cintie.musicrecommender.spotifyservice.clients.SpotifyApiClient;
 import com.cintie.musicrecommender.spotifyservice.dto.TrackVector;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,6 +11,10 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class SpotifyRecommendationService {
+
+    private final SpotifyApiClient spotifyApiClient;
+    private final AuthServiceClient authServiceClient;
+    private final SpotifyRecommendationCacheService spotifyRecommendationCacheService;
 
     public List<TrackVector> getRecommendations(String spotifyId){
         
